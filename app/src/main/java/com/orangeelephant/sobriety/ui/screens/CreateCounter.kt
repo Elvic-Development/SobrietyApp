1554d83def348e1af977df10a49e18fbce706d6d
Toadsta <78148898+Toadsta@users.noreply.github.com>
2023-06-13 15:51:35 +0100

    Added date button still WIP

--- app/src/main/java/com/orangeelephant/sobriety/ui/screens/CreateCounter.kt
+++ app/src/main/java/com/orangeelephant/sobriety/ui/screens/CreateCounter.kt
@@ -1,6 +1,10 @@
 package com.orangeelephant.sobriety.ui.screens
 
-import android.provider.Settings.Global.getString
+
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 
 import androidx.compose.material3.*
@@ -10,18 +14,27 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.getString
-import androidx.core.content.res.TypedArrayUtils.getString
+
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
+import com.maxkeppeler.sheets.calendar.CalendarDialog
+import com.maxkeppeler.sheets.calendar.models.CalendarSelection
+
+
 import com.orangeelephant.sobriety.R
 import com.orangeelephant.sobriety.ui.common.BackIcon
 import com.orangeelephant.sobriety.ui.common.GenericTopAppBar
 
+
+
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateCounter(navController: NavController) {
@@ -49,10 +62,24 @@ fun CreateCounter(navController: NavController) {
 
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Creation() {
     val context = LocalContext.current
     var text by remember { mutableStateOf("") }
+    var isDialogOpen by remember { mutableStateOf(false) }
+
+
+    val calendarState = rememberUseCaseState()
+
+    CalendarDialog(
+        state= calendarState,
+        selection = CalendarSelection.Date {
+            date ->  Log.d("SelectedDate", "$date")
+        }
+    )
+
 
     Column(
         verticalArrangement = Arrangement.Bottom,
@@ -70,9 +97,10 @@ fun Creation() {
             modifier = Modifier
                 .fillMaxWidth(),
             value = text,
-            onValueChange = { text = it },
+            onValueChange = { text = calendarState.toString() },
             label = { Text(context.getString(R.string.placeholder_counter_name)) }
         )
+
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -82,6 +110,18 @@ fun Creation() {
             style = MaterialTheme.typography.titleMedium,
         )
 
+        Button(onClick = { calendarState.show() }) {
+            Text(text = "Test")
+        }
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(context.getString(R.string.placeholder_date))}
+        )
+
         Spacer(modifier = Modifier.height(16.dp))
 
         // reason
@@ -89,6 +129,7 @@ fun Creation() {
             text = context.getString(R.string.create_counter_reason),
             style = MaterialTheme.typography.titleMedium,
         )
+
 
         OutlinedTextField(
             modifier = Modifier
@@ -100,7 +141,10 @@ fun Creation() {
     }
 }
 
+
+
 // create compostable preview
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 fun CreateCounterPreview() {
